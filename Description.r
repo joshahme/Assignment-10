e27df5d9672221f713899b1798da28b3479ff118
joshahme <155593968+joshahme@users.noreply.github.com>
2024-03-15 09:58:19 -0400

    Description
    In this package, it provide function to analyze and visualize sales data towards different products over a period of time . It includes functionality to calculate total sales for each product and to visualize them using bar plots, as well as to analyze average sales per month and visualize them using line plots.. This package is useful towards gaining insights into sales trends and performance over time.

---
+++ Description.r
@@ -0,0 +1,8 @@
+Package: SalesAnalysis
+Title: Sales Analysis and Visualization
+Version: 0.1
+Authors@R: "joshah <joshah@usf.edu> [aut, cre]"
+Description: This package provides functions to analyze and visualize sales data for different products over a period of time. It includes functionality to calculate total sales for each product and visualize them using bar plots, as well as to analyze average sales per month and visualize them using line plots. This package is useful for gaining insights into sales trends and performance over time.
+Depends: R (>= 3.6.0), ggplot2, dplyr
+License: MIT
+LazyData: true
